1b16548aaf5fab1cf74e996b5cb295cee6070210
Jwainlee <mudom9@naver.com>
2023-02-10 20:32:42 +0900

    Update 230210-print.py

--- 230210-print.py
+++ 230210-print.py
@@ -1,2 +1,15 @@
-print('Hello')
-print('World')
+# dict 객체를 이용하는 단어 빈도수 구하기
+
+char = ['jaein', 'jimin', 'jaein', 'jimin', 'dayeon']
+bindo = {}
+
+# get()
+
+for key in char :
+    bindo[key] = bindo.get(key, 0) + 1
+
+print(bindo)
+
+
+
+
