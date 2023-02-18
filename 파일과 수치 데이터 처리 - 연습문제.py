0dc92f9f480aa421ab586fe688fc39ae88312fb6
Jwainlee <mudom9@naver.com>
2023-02-18 21:58:59 +0900

    Update 파일과 수치 데이터 처리 - 연습문제.py

--- 파일과 수치 데이터 처리 - 연습문제.py
+++ 파일과 수치 데이터 처리 - 연습문제.py
@@ -8,8 +8,37 @@ print(emp)
 print('관측치 길이: ', len(emp))
 print('전체 평균 급여: ', emp['Pay'].mean())
 
-mi = min(emp['Pay'])
-ma = max(emp['Pay'])
+p = emp['Pay']
+n = emp['Name']
+mi = min(p)
+ma = max(p)
 
-for i in len(emp) :
-    
+
+for idx in range(len(emp)) :
+    if  p[idx] == mi :
+        print('최저 급여: %d, 이름: %s' %(p[idx], n[idx]))
+    elif p[idx] == ma :
+        print('최고 급여: %d, 이름: %s' %(p[idx], n[idx]))
+
+
+# 2
+import numpy as np
+arr2 = np.random.randn(5, 4)
+arr2
+
+
+for i in range(len(arr2[:,])) :
+    idx = i + 1
+    a = arr2[i,]
+    print('%d행 합계: %d' %(idx, sum(a)))
+    print('%d행 최대값: %d' %(idx, max(a)))
+    print('%d행 최소값: %d' %(idx, min(a)))
+
+
+# 3
+
+
+
+
+
+
