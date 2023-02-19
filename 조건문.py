8cbbd0b4473cc5a8e2f5d3dd4fb25aa02f234f35
Jwainlee <mudom9@naver.com>
2023-02-19 19:52:02 +0900

    Update 조건문.py

--- 조건문.py
+++ 조건문.py
@@ -1,126 +1,89 @@
-var1=100
+var1 = 100
 print(id(var1))
 print(type(var1))
 
 
 # 실수-> 정수
-a=int(10.5)
-b=int(20.42)
-add=a+b
-print('add=', add)
-add= 30
+a = int(10.5)
+b = int(20.42)
+add = a + b
+print('add = ', add)
 
-a=float(10)
-b=float(20)
-add2=a+b
-print('add2=', add2)
-add2= 30.0
+a = float(10)
+b = float(20)
+add2 = a + b
+print('add2 = ', add2)
 
-a**2
-100.0
+a ** 2
+
 
 #여러 줄 문자열
-multiline=""" Hi
+multiline = """ Hi
 my name is
 Jae In """
 print(multiline)
- Hi
-my name is
-Jae In 
+
 
 multiline2="Hi\nmy name is\nJae In"
 print(multiline2)
-Hi
-my name is
-Jae In
+
 
 #문자열 색인
 string = "PYTHON"
 print(string[0])
-P
+
 print(string[1])
-Y
+
 print(string[-5])
-Y
+
 
 #문자열 연산
-print("I am"+ str(24) + "years old")
-I am24years old
+print("I am " + str(24) + "years old")
 
-print("가지마"*20)
-가지마가지마가지마가지마가지마가지마가지마가지마가지마가지마가지마가지마가지마가지마가지마가지마가지마가지마가지마가지마
+
+print("가지마" * 20)
+
 
 #나머지 계산
-div1 = 10*3
+div1 = 10 % 3
 print(div1)
-30
-# 이게 아닌데
-div1 = 10%3
-print(div1)
-1
+
 
 # 몫 계산
 div2 = 10//3
 print(div2)
-3
+
 10.3%2.5
-0.3000000000000007
 
 
 # 논리연산자
-result1 = 30>50
+result1 = 30 > 50
 print(result1)
-False
-result2 = 30>50 or 70>50
+
+result2 = 30 > 50 or 70 > 50
 print(result2)
-True
+
 
 # 대입연산자
 i = tot = 10
 i += 1
 tot += i
 print(i, tot)
-11 21
 
-print('재인' '민지', end=',')
-재인민지,
-print('재인','민지', end=',')
-재인 민지,
-print('재인', end=',')
-재인,
-print('민지')
-민지
-print('출력1', end=',')
-출력1,
-print('출력2')
-출력2
-print('출력1', end=',')
-출력1,
-
-print(i, tot, end=',')
-11 21,
-print('재인''민지', end=',')
-재인민지,
-print('재인, ''민지')
-재인, 민지
 
 # 두 변수의 값 교체
 v1, v2 = 100, 200
 v2, v1 = v1, v2
 print(v1, v2)
-200 100
 v3, v1 = v1, v2
 print(v1,v2,v3)
-100 100 200
 
 # 패킹 할당 연산자
 lst = [1,2,3,4,5]
 v1, *v2 = lst
-print(v1, v2)
-1 [2, 3, 4, 5]
+print(v1, v2)       # 1 [2, 3, 4, 5]
 *v1, v2 = lst
-print(v1, v2)
-[1, 2, 3, 4] 5
+print(v1, v2)       # [1, 2, 3, 4] 5
 
 
 # 조건문 - 단일 조건문
@@ -129,38 +92,13 @@ if var >= 5 :
     print('var = ', var)
     print('var는 5보다 크다.')
     print('조건 만족')
-print('항상 실행')
-SyntaxError: invalid syntax
-if var >= 5 :
-    print('var = ', var)
-    print('var는 5보다 크다.')
-    print('조건 만족')
-print('항상 실행')
-SyntaxError: invalid syntax
-if var >= 5 :
-    print('var = ', var)
-    print('var는 5보다 크다.')
-    print('조건 만족')
 
+print('항상 실행')
     
-var =  10
-var는 5보다 크다.
-조건 만족
-if var >= 5 :
-    print('var = ', var)
-    print('var는 5보다 크다.')
-    print('조건 만족')
-
-    
-var =  10
-var는 5보다 크다.
-조건 만족
 
 # 100~85: 우수, 84~70: 보통, 69이하: 저조
-
 score = int(input('점수 입력: '))
-if score >= 85 and score <= 100
-SyntaxError: incomplete input
+
 if score >= 85 and score <= 100 :
     print('우수')
 else :
