13f326391b90faa72fcd7fb1c15c151692cf550a
Jwainlee <mudom9@naver.com>
2023-04-09 21:00:10 +0900

    Update 로또추첨(1~9, 5자리).py

--- 로또추첨(1~9, 5자리).py
+++ 로또추첨(1~9, 5자리).py
@@ -2,19 +2,26 @@ import random
 random_extr = random.randint(0,9)
 
 lotto = []
-random_sel = []
+user = []
+random_sel = int(input('번호를 입력하세요(1~9): '))
 
 
 for i in range(5):
     while random_extr in lotto:
         random_extr = random.randint(0,9)
     lotto.append(random_extr)
-    
-    random_sel.append(int(input('번호를 입력하세요(1~9): ')))
 
+    while random_sel in user:
+        random_sel = int(input('번호를 입력하세요(1~9): '))
+    user.append(random_sel)
+    
+    
 for i in range(5) :
-    if random_sel[i] in lotto :
-        print(random_sel[i])
+    if user[i] in lotto :
+        print(user[i])
+
+
+
 
 
 
